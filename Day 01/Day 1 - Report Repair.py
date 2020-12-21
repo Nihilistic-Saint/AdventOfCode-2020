5c413c11f9abcd956c720542c8d756c2756e21ba
Nikolas <nik.baez1@gmail.com>
2020-12-21 17:05:26 +0200

    Finished code!

--- Day 01/Day 1 - Report Repair.py
+++ Day 01/Day 1 - Report Repair.py
@@ -1,4 +1,5 @@
 '''
+--- Part One ---
 Find two values from input.txt that sum to 2020 and then multiply those two numbers together.
 '''
 
@@ -11,38 +12,35 @@ for values in text:
     data.append(int(values))
 
 # Creating list
-def function (list):
-
+def function1 (list):
     # Finding the two values
-    x = 0
-    for z in range(len(list)):
-        for y in range(len(list)):
-            output = list[x] + list[y]
+    for z in range(len(list) - 1):
+        for y in range(z + 1, len(list)):
+            output = list[z] + list[y]
             if output == 2020:
-                print(f"Index:{str(x)} \nIndex:{str(y)} \nEquals: 2020")
-                print(f"{list[x]} + {list[y]} = 2020\n")
-
-                # Multiplying values
-                print(f"{list[x]} * {list[y]} = {list[x] * list[y]}")
-        x += 1
-
-def part2(list):
-    x = 1
-    for z in range(len(list)):
-        for y in range(len(list)):
-            output = list[x] + list[y]
-            print(f"{str(x)} <-> {str(y)} -> {str(z)}\n")
-
-            if output == 2020:
-                print(f"Index:{str(x)} \nIndex:{str(y)} \nEquals: 2020")
-                print(f"{list[x]} + {list[y]} = 2020\n")
-
-                # Multiplying values
-                print(f"{list[x]} * {list[y]} = {list[x] * list[y]}")
-        x += 1
+                print("\n---Part One---")
+                print(f"Index:\t{str(z)}\t\t{str(y)}")
+                print(f"Values:\t{list[z]}\t + \t{list[y]} = {list[z] + list[y]}\n")
+                return (f"Multiplying values:\n{list[z]} * {list[y]} = {list[z] * list[y]}")
 
 
-#print(function(data), "\n")
 
-print(part2(data))
+'''
+--- Part Two ---
+Find three values from input.txt that sum to 2020 and then multiply those three numbers together.
+'''
 
+def function2(list):
+    for z in range(len(list) - 2):
+        for y in range(z + 1, len(list)):
+            for x in range(y + 1, len(list)):
+                output = list[z] + list[y] + list[x]
+                if output == 2020:
+                    print("\n---Part Two---")
+                    print(f"Index:\t{str(z)}\t\t{str(y)}\t\t{str(x)}")
+                    print(f"Values:\t{list[z]}\t + \t{list[y]} + \t{list[x]} = {list[z] + list[y] + list[x]}\n")
+                    return (f"Multiplying values:\n{list[z]} * {list[y]} * {list[x]} = {list[z] * list[y] * list[x]}\n")
+
+
+print(function1(data))
+print(function2(data))

0bfb5523cc96d63c2fe154892ac5fb7f3b16865d
Nikolas <nik.baez1@gmail.com>
2020-12-22 15:24:19 +0200

    Solution to Day 2 part 1 & 2

--- Day 02/Day 2 - Password Philosophy.py
+++ Day 02/Day 2 - Password Philosophy.py
@@ -7,6 +7,7 @@ The policy indicates the minimum and maximum number of times a given letter must
 import csv
 
 def part1():
+    print("--- Part One ---")
     with open('input.csv') as data:
         reader = csv.reader(data, delimiter=' ')
         lines = 0
@@ -15,24 +16,18 @@ def part1():
 
         for line in reader:
             demand, letter, password = line[0], line[1][0], line[2]
-            print(line)
 
             i = demand.index('-')
             min = int(demand[:i])
             max = int(demand[i+1:])
-            #print(min, max, demand)
             count = 0
             for x in line[2]:
                 #print(x)
                 if x == letter:
                     count += 1
             if count >= min and count <= max:
-                print(f"Password is correct.\n")
-                #print(count)
                 correct += 1
             else:
-                print("Password is INCORRECT.\n")
-                #print(count)
                 incorrect += 1
             lines += 1
 
@@ -46,22 +41,31 @@ Now the policy indicates a position a given letter has to be in.
 One correct letter is enough for a valid password.
 '''
 def part2():
+    print("--- Part Two ---")
     with open('input.csv') as data:
         reader = csv.reader(data, delimiter=' ')
+        count = 0
+        incorrect = 0
         for line in reader:
             demand, letter, password = line[0], line[1][0], line[2]
-            print(line)
 
             i = demand.index('-')
             x = int(demand[:i])
             y = int(demand[i + 1:])
-            one = int(demand[x])
-            two = int(demand[y])
-            print(x, y, demand)
-            print(one, two)
-            return ''
 
-#print(part1())
+            one = password[x - 1] == letter
+            two = password[y - 1] == letter
+
+            if (one and not two) or (two and not one):
+                count += 1
+            elif letter == one and two:
+               incorrect += 1
+            else:
+                incorrect += 1
+        print(f"Total of valid passwords -> {count}")
+        return (f"Total of invalid passwords -> {incorrect}")
+
+print(part1())
 print(part2())
 
 
